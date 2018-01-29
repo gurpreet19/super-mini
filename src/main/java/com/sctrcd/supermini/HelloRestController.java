6d28f56052c110e10dccf5f415dbf53e80f21a53
gurpreet19 <gurpreets705@gmail.com>
2018-01-29 22:24:16 +0530

    test

--- src/main/java/com/sctrcd/supermini/HelloRestController.java
+++ src/main/java/com/sctrcd/supermini/HelloRestController.java
@@ -10,7 +10,7 @@ public class HelloRestController {
     
     public HelloRestController() {
     }
-    //random stuff here
+    //random stuff here1
     @RequestMapping(value = "/hello")
     public String countries() {
         return "Hello, World!";
